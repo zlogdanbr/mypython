e6ba9025713e0498a78b2e461dc580306d86b39f
Daniel <54685402+zlogdanbr@users.noreply.github.com>
2023-08-31 11:31:38 -0300

    Update ebook.py
    improved with instructions

--- src/ebook.py
+++ src/ebook.py
@@ -1,3 +1,31 @@
+'''
+Removing DRM
+
+Step 1 Download calibre         https://calibre-ebook.com
+Step 2 Install DRM plugins      https://www.epubor.com/calibre-drm-removal-plugins.html
+Step 3 Set the paths below
+CALIBRE_FOLDER = "C:\\Users\\Administrador\\Biblioteca do calibre"
+KINDLE_FOLDER = "C:\\Users\\Administrador\\Documents\\My Kindle Content"
+OUT_DIR = "C:\\Users\Administrador\\Documents\\tmp"
+Step 4 Add the desired action to main
+    4.1 copyfiles(CALIBRE_FOLDER,AZWEXT)
+    4.2 convert_batch(CALIBRE_FOLDER,AZWEXT3,ZIPEXT)
+    4.3 change_extension(OUT_DIR,ZIPEXT,CBZEXT)
+    4.4 remove_files(OUT_DIR,EPUBEXT)
+    
+For comics:    
+I usually copy  copyfiles(KINDLE_FOLDER,AZWEXT)  
+Then I manually add them to calibre ( drag and drop )
+
+Calibre will create a decent folder structure at  CALIBRE_FOLDER
+I call convert_batch(CALIBRE_FOLDER,AZWEXT3,ZIPEXT) and all zip files will be placed at
+OUT_DIR
+
+
+Then change_extension(OUT_DIR,ZIPEXT,CBZEXT) will simply rename the zip files to cbz
+allowing any reader to open 
+'''
+
 import sys
 import os
 import pathlib
@@ -11,8 +39,22 @@ KINDLE_FOLDER = "C:\\Users\\Administrador\\Documents\\My Kindle Content"
 OUT_DIR = "C:\\Users\Administrador\\Documents\\tmp"
 
 ZIPEXT = "zip"
-CBZEXT = ".cbz"
+CBZEXT = "cbz"
 AZWEXT = "azw"
+AZWEXT3 = "azw3"
+EPUBEXT = "epub"
+
+'''
+calls ebook-convert.exe to convert myfile.input_format to myfile.output_format
+'''
+def convert_files( orig, dest ):
+
+    print("Running ebook-convert for "+orig+" "+dest)
+    cmd = []
+    cmd.append("ebook-convert")
+    cmd.append(orig)
+    cmd.append(dest)
+    run_win_cmd(cmd)
 
 '''
 executes a commmand line cmd 
@@ -25,12 +67,22 @@ def run_win_cmd(cmd):
         print(result.stderr)
     else:
         print(result.stdout)
+        
+def convert_batch( dir, ext_orig, ext_final):
+
+    for root, dirs, files in os.walk(dir):
+        for name in files:
+            fext = getextension(name)
+            file_name, t= os.path.splitext(name)
+            if ( fext == ext_orig):
+                final = "{}.{}".format(file_name, ext_final)
+                convert_files(root+"\\"+name,OUT_DIR+"\\"+final)
 
 '''
-rename all files under the directory dir ( including subfolders ) and changes the extensions
+changes the extension all files under the directory dir ( including subfolders ) 
 from ext to myextfinal
 '''
-def renamefiles(dir,ext,myextfinal):
+def change_extension(dir,ext,myextfinal):
 
     for root, dirs, files in os.walk(dir):
         for name in files:
@@ -50,12 +102,12 @@ def copyfiles(dir,ext):
             full = "{}\{}".format(root,name)
             if ( fext == ext):
                 print("copying {}".format(full))
-                copy( full,outdir)
+                copy( full,OUT_DIR)
  
 '''
 Removes all files under the directory dir ( including subfolders ) who extension is not ext
 ''' 
-def remove_files(dir, ext):
+def remove_files(dir,ext):
 
     for root, dirs, files in os.walk(dir):
         for name in files:
@@ -69,7 +121,7 @@ def remove_files(dir, ext):
 copy a file src to dst
 dst can be a new file name or a directory
 '''
-def copy(src, dst):
+def copy(src,dst):
 
     if os.path.isdir(dst):
         dst = os.path.join(dst, os.path.basename(src))
@@ -88,12 +140,14 @@ Changes the extension of a file to ext
 def changeExt( file, ext ):
 
     pre, fext = os.path.splitext(file)
-    os.rename( file, pre + ext )
+    os.rename( file, pre + "." + ext )
 
 def main(argv):
-
-    renamefiles(CALIBRE_FOLDER,ZIPEXT,CBZEXT)
-
+    #copyfiles(CALIBRE_FOLDER,AZWEXT)
+    #convert_batch(CALIBRE_FOLDER,AZWEXT3,ZIPEXT)
+    #change_extension(OUT_DIR,ZIPEXT,CBZEXT)
+    #remove_files(OUT_DIR,EPUBEXT)
+    pass
 
 if __name__ == '__main__':
 
